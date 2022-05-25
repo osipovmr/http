82e27ca943cece450a1fe9a10b845d3d082f3ff4
osipovmr <osipovmr>
2022-05-25 22:53:34 +0300

    no working

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -1,30 +1,22 @@
 import java.sql.*;
 
-import static spark.Spark.*;
+public class LogIn {
+    public static String url = "jdbc:postgresql://c-c9qkc196nr3nge59qclr.rw.mdb.yandexcloud.net:6432/students-project";
+    public static String login = "students-project";
+    public static String password = "kd5HdsI2fcidKsqC";
+    public static Statement statement = null;
+    public static ResultSet result;
+    public static Query query = new Query();
+    public static Airports airports = new Airports();
 
-public class HelloWorld {
-
-
-    public static void main(String[] args) {
-        LogIn logIn = new LogIn();
-        Airports airports = new Airports();
-        Query query = new Query();
-        get("/hello", (req, res) -> logIn.connection()
-                + "<br>"
-                + query.query()
-                + "<br>"
-                + airports.getAirports());
-
-        /*
-           Airports airports = new Airports();
-        Statement statement = null;
-        String query = "select * from bookings.airports";
+    public static String connection() {
         ResultSet result;
+        Statement statement = null;
         try (Connection conn = DriverManager.getConnection(url, login, password)) {
             if (conn != null) {
                 System.out.println("Connected to the database!");
+                result = statement.executeQuery(query.query());
                 statement = conn.createStatement();
-                result = statement.executeQuery(query);
                 while (result.next()) {
                     System.out.println(result.getString("airport_code") + "\t " + result.getString("airport_name")
                             + "\t" + result.getString("city")
@@ -38,22 +30,17 @@ public class HelloWorld {
                             + "\t" + result.getString("timezone")
                             + "<br>");
                 }
-            } else {
-                System.out.println("Failed to make connection!");
+                return "Connected to the database!"
+                        + "<br>"
+                        + url;
             }
+
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        get("/hello", (req, res) -> "Алена, привет!"
-                + "<br>"
-                + "А вот и таблица!"
-                + "<br>"
-                + airports.getAirports()); */
-
-
+        return "Failed to make connection!";
     }
 }
+
---
+++ src/main/java/Query.java
@@ -0,0 +1,9 @@
+public class Query {
+    public String query() {
+        return "select * from bookings.airports";
+    }
+
+    ;
+
+
+}
