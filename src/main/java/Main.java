beb58a821fcb53c491bd5335de8086e6f5aa15a6
osipovmr <osipovmr>
2022-06-29 23:42:42 +0300

    editing 29/06/2022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,51 +3,44 @@ import static spark.Spark.*;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class HelloWorld {
+public class Main {
     //закидываем все классы
     public static Statement statement = null;
     public static Container container = new Container();
-    public static AirportInfo airportInfo = new AirportInfo();
     public static TicketInfo ticketInfo = new TicketInfo();
     public static CodeInfo codeInfo = new CodeInfo();
     public static String check = null;
     static ResultSet resultAirport;
     static ResultSet resultTicket;
     static ResultSet resultCode;
-    public static String queryAirport = "select * from bookings.airports"; // вызов всей таблицы
     //1. для конкретного номера билета (ticket_no) узнать имя пассажира, тариф, статус, город вылета.
-    public static String queryTicket = "select tickets.ticket_no, "
-            + "passenger_name, "
-            + "fare_conditions, "
-            + "status, "
-            + "city"
-            + "\n"
-            + "from bookings.tickets"
-            + "\n" +
-            "inner join bookings.ticket_flights on bookings.tickets.ticket_no = bookings.ticket_flights.ticket_no"
-            + "\n" +
-            " inner join bookings.flights on bookings.ticket_flights.flight_id = bookings.flights.flight_id"
-            + "\n" +
-            "inner join bookings.airports on bookings.flights.departure_airport = bookings.airports.airport_code"
-            + "\n" +
-            "limit 100";
+    public static String queryTicket = """
+            select tickets.ticket_no,passenger_name, fare_conditions, status, city
+            from bookings.tickets
+            inner join bookings.ticket_flights on bookings.tickets.ticket_no = bookings.ticket_flights.ticket_no
+            inner join bookings.flights on bookings.ticket_flights.flight_id = bookings.flights.flight_id
+            inner join bookings.airports on bookings.flights.departure_airport = bookings.airports.airport_code
+            limit 100
+            """;
     //2. для конкретного кода аэропорта (airport_code) узнать количество вылетов из этого аэропорта,
     // среднюю стоимость перелета из этого аэропорта, количество пассажиров, вылетевших из аэропорта
-    public static String queryCode = "select airport_code, " +
-            "count (\"Вылетевший рейс\") as \"Кол-во вылетевших рейсов\"," +
-            "sum (\"Кол-во пассажиров в рейсе\") as \"Кол-во вылетевших пассажиров\"," +
-            "sum(\"Средняя стоимость билета\")/count (\"Вылетевший рейс\") as \"Средняя стоимость вылета\"\n" +
-            "from (\n" +
-            "select a.airport_code, departure_airport, f.flight_id as \"Вылетевший рейс\", count (t.ticket_no) as \"Кол-во пассажиров в рейсе\",  round (avg (amount), 2) as \"Средняя стоимость билета\"\n" +
-            "from bookings.airports a \n" +
-            "inner join bookings.flights f on a.airport_code = f.departure_airport \n" +
-            "inner join bookings.ticket_flights tf on f.flight_id = tf.flight_id \n" +
-            "inner join bookings.tickets t on tf.ticket_no =t.ticket_no \n" +
-            "where status = 'Departed'\n" +
-            "group by f.flight_id, a.airport_code \n" +
-            "order by f.flight_id ) as qwe\n" +
-            "group by qwe.airport_code\n" +
-            "order by airport_code";
+    public static String queryCode = """
+            select airport_code,
+            count (\"Вылетевший рейс\") as \"Кол-во вылетевших рейсов\",
+            sum (\"Кол-во пассажиров в рейсе\") as \"Кол-во вылетевших пассажиров\",
+            sum(\"Средняя стоимость билета\")/count (\"Вылетевший рейс\") as \"Средняя стоимость вылета\"
+            from (
+            select a.airport_code, departure_airport, f.flight_id as \"Вылетевший рейс\", count (t.ticket_no) as \"Кол-во пассажиров в рейсе\",  round (avg (amount), 2) as \"Средняя стоимость билета\"
+            from bookings.airports a
+            inner join bookings.flights f on a.airport_code = f.departure_airport
+            inner join bookings.ticket_flights tf on f.flight_id = tf.flight_id
+            inner join bookings.tickets t on tf.ticket_no =t.ticket_no
+            where status = 'Departed'
+            group by f.flight_id, a.airport_code
+            order by f.flight_id ) as qwe
+            group by qwe.airport_code
+            order by airport_code
+            """;
 
     public static String connection() {
         try {
@@ -65,33 +58,6 @@ public class HelloWorld {
             check = "Ошибка SQL!";
         }
         return check;
-    }
-
-    public static ArrayList airport() {
-        try {
-            ArrayList<String> a = new ArrayList<>();
-            resultAirport = statement.executeQuery(queryAirport);
-            while (resultAirport.next()) {
-                airportInfo.AirportsInfo(resultAirport.getString("airport_code"),
-                        resultAirport.getString("airport_name"),
-                        resultAirport.getString("city"),
-                        resultAirport.getString("longitude"),
-                        resultAirport.getString("latitude"),
-                        resultAirport.getString("timezone")
-                        );
-                a.add(airportInfo.toString());
-                String key = resultAirport.getString("airport_code");
-                String value = airportInfo.toString();
-                container.setJedis(key, value);
-            }
-            System.out.println("База airport собрана!");
-            return a;
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public static ArrayList ticket() {
@@ -146,12 +112,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(connection());
         System.out.println(container.redisTester());
-        airport();
         ticket();
         code();
 
         get("/hello", (req, res) -> check);
-        get("/airport", (req, res) -> airport());
         get("/ticket", (req, res) -> ticket());
         get("/code", (req, res) -> code());
     }
