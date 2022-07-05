daf9848e0d023e031d55fc4d36e35381ec3aaf0d
osipovmr <osipovmr>
2022-07-05 23:05:48 +0300

    add working url parameter to SQL method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,25 @@
 import static spark.Spark.*;
 
 public class Main {
-    //закидываем все классы
+
     public static ContainerRedis container = new ContainerRedis();
 
     public static void main(String[] args) {
         System.out.println(PostgresData.connection());
         System.out.println(container.redisTester());
-        System.out.println(PostgresData.airport());
-        System.out.println(PostgresData.ticket());
 
         get("/hello", (req, res) -> PostgresData.connection());
-        get("/ticket", (req, res) -> PostgresData.ticket() );
-        get("/airport", (req, res) -> PostgresData.airport());
 
+        get("/ticket", (request, response) -> {
+            String ticketNo;
+            String param = request.queryParams("no");
+            ticketNo = "'"+param+"'";
+            return PostgresData.ticket(ticketNo);});
+
+        get("/airport", (request, response) -> {
+            String code;
+            String param = request.queryParams("code");
+            code = "'"+param+"'";
+            return PostgresData.airport(code);});
     }
-
 }
